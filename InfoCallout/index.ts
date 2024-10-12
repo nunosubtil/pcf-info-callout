d1633372d20fc0c7701010e414ad9361980d8a84
nunosubtil <150437999+nunosubtil@users.noreply.github.com>
2024-10-12 17:50:45 +0100

    added isDisabled and context

--- InfoCallout/index.ts
+++ InfoCallout/index.ts
@@ -1,56 +1,43 @@
 import { IInputs, IOutputs } from "./generated/ManifestTypes";
-import { HelloWorld, IHelloWorldProps } from "./HelloWorld";
 import * as React from "react";
+import InfoCallout from "./InfoCallout";
 
-export class InfoCallout implements ComponentFramework.ReactControl<IInputs, IOutputs> {
-    private theComponent: ComponentFramework.ReactControl<IInputs, IOutputs>;
+export class PCFInfoCallout implements ComponentFramework.ReactControl<IInputs, IOutputs> {
     private notifyOutputChanged: () => void;
+    private _isDisabled: boolean;
+    private context: ComponentFramework.Context<IInputs>;
 
-    /**
-     * Empty constructor.
-     */
-    constructor() { }
+    constructor() {}
 
-    /**
-     * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
-     * Data-set values are not initialized here, use updateView.
-     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
-     * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
-     * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
-     */
     public init(
         context: ComponentFramework.Context<IInputs>,
         notifyOutputChanged: () => void,
         state: ComponentFramework.Dictionary
     ): void {
         this.notifyOutputChanged = notifyOutputChanged;
+        this.context = context;
+        this._isDisabled = context.mode.isControlDisabled;
     }
 
-    /**
-     * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
-     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
-     * @returns ReactElement root react element for the control
-     */
     public updateView(context: ComponentFramework.Context<IInputs>): React.ReactElement {
-        const props: IHelloWorldProps = { name: 'Hello, World!' };
-        return React.createElement(
-            HelloWorld, props
-        );
+        this.context = context;
+        this._isDisabled = context.mode.isControlDisabled;
+
+        const props = {
+            labelText: context.parameters.labelText.raw || "",
+            labelColor: context.parameters.labelColor?.raw || "#000000",
+            isBold: context.parameters.isBold?.raw || false,
+            isItalic: context.parameters.isItalic?.raw || false,
+        };
+
+        return React.createElement(InfoCallout, props);
     }
 
-    /**
-     * It is called by the framework prior to a control receiving new data.
-     * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as "bound" or "output"
-     */
     public getOutputs(): IOutputs {
-        return { };
+        return {};
     }
 
-    /**
-     * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
-     * i.e. cancelling any pending remote calls, removing listeners, etc.
-     */
     public destroy(): void {
-        // Add code to cleanup control if necessary
+        // Cleanup if necessary
     }
 }
